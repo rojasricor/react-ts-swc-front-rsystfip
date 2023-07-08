d4f8c404bec683d70f2e948ce1456eb083cd667e
rojasricor <rojasricor@gmail.com>
2023-07-08 17:16:58 -0500

    constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const API_ROUTE = "http://localhost:3000/api";
 export const AUTH_KEY = "RSystfip_user_authenticated";
 export const UNSET_STATUS = "unset";
-export const TOAST_ID = "toast-warn-errors-api";
+export const TOAST_ID = "toast-content-rsystfip";
